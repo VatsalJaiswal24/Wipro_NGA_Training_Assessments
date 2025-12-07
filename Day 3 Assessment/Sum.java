5cf14f82abfcedb86366da64b089c417a6fad849
Vatsal Jaiswal <Vatsaljaiswal1@gmail.com>
2025-12-07 17:24:38 +0530

    Add files via upload

---
+++ Day 3 Assessment/Sum.java
@@ -0,0 +1,24 @@
+// Done by Vatsal Jaiswal
+/*Sum of All Numbers
+Find the sum of the digits using recursion */
+package Day3;
+
+import java.util.Scanner;
+
+public class Sum {
+
+    public static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int result = sumOfDigits(number);
+        System.out.println(result);
+        scanner.close();
+    }
+}
